8a419d8dbe0f6e16c45097644eea98f1d08c4912
daknya_bam <daknyabam18@gmil.com>
2021-12-03 14:53:01 +0530

    oush

--- learn.js
+++ learn.js
@@ -1,38 +1,3 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.link');
 
-    
+
  
-     burger.addEventListener('click',()=>{
-       nav.classList.toggle('nav-active');
-
-       burger.classList.toggle('toggle');
-     });
-
-     
-    
-  }
-  navSlide();
-
-  function openPage(pageName, elmnt, color) {
-    
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-   
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-    }
-  
-    tablinks = document.getElementsByClassName("tablink");
-   for (i = 0; i < tablinks.length; i++) {
-     tablinks[i].style.backgroundColor = "";
-   }
-  
- 
-    document.getElementById(pageName).style.display = "block";
-  
-   elmnt.style.backgroundColor = color;
-  }
-  
-  document.getElementById("defaultOpen").click();
